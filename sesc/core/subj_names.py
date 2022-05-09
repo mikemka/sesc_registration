143bc779415c826715ead1e554e31d3dbd8daf49
микемка <mikemka@vk.com>
2022-05-10 00:35:33 +0500

    add excel without user list

--- sesc/core/subj_names.py
+++ sesc/core/subj_names.py
@@ -1,95 +1,168 @@
-SUBJ_NAMES = {
-    "10 гум": {
-        'd1': 'Второй иностранный язык (Б)',
-        'd2': 'География (Б)',
-        'd3': 'Обществознание (Б)',
-        'd4': 'Теория познания (Б)',
-        'd5': 'Информатика (Б)',
-        'd6': 'Естествознание (Б)',
-        'd7': 'Диалоги с Чеховым',
-        'd8': 'Современная литература в жанровом аспекте',
-        'd9': 'Риторика',
-        'd10': 'История философии',
-        'd11': 'История мировой культуры',
-    },
-    "10 соцгум": {
-        'd1': 'География (Б)',
-        'd2': 'Теория познания (Б)',
-        'd3': 'МХК (Б)',
-        'd4': 'Информатика (Б)',
-        'd5': 'Естествознание (Б)',
-        'd6': 'Зарубежная литература',
-        'd7': 'Современная литература в жанровом аспекте',
-        'd8': 'Диалоги с Чеховым',
-        'd9': 'Политология',
-        'd10': 'Социология',
-        'd11': 'История философии',
-    },
-    "10 соцэк": {
-        'd1': 'Теория познания (Б)',
-        'd2': 'МХК (Б)',
-        'd3': 'География (Б)',
-        'd4': 'Информатика (Б)',
-        'd5': 'Естествознание (Б)',
-        'd6': 'Экономическая география',
-        'd7': 'История философии',
-        'd8': 'Сложные математические задачи',
-    },
-    '10 матинф': {
-        'd1': 'Обществознание (Б)',
-        'd2': 'География (Б)',
-        'd3': 'МХК (Б)',
-        'd4': 'Теория познания (Б)',
-        'd5': 'Биология (Б)',
-        'd6': 'Физика (Б)',
-        'd7': 'Химия (Б)',
-        'd8': 'Избранные главы учебников естественных наук',
-    },
-    '10 физмат': {
-        'd1': 'Обществознание (Б)',
-        'd2': 'География (Б)',
-        'd3': 'МХК (Б)',
-        'd4': 'Теория познания (Б)',
-        'd5': 'Биология (Б)',
-        'd6': 'Химия (Б)',
-        'd7': 'Астрономия',
-        'd8': 'Избранные главы учебников естественных наук',
-    },
-    '10 физтех': {
-        'd1': 'Обществознание (Б)',
-        'd2': 'География (Б)',
-        'd3': 'МХК (Б)',
-        'd4': 'Теория познания (Б)',
-        'd5': 'Биология (Б)',
-        'd6': 'Химия (Б)',
-        'd7': 'Инженерная графика (геометрическое и проекционное черчение)',
-        'd8': 'Астрономия',
-        'd9': 'Избранные главы учебников естественных наук',
-    },
-    '10 хим': {
-        'd1': 'Обществознание (Б)',
-        'd2': 'География (Б)',
-        'd3': 'МХК (Б)',
-        'd4': 'Теория познания (Б)',
-        'd5': 'Информатика (Б)',
-        'd6': 'Биология (Б)',
-        'd7': 'Решение сложных задач по математике',
-        'd8': 'Физические задачи повышенной сложности',
-        'd9': 'Биологические задачи',
-        'd10': 'Химический практикум',
-        'd11': 'Астрономия',
-    },
-    '10 био': {
-        'd1': 'Обществознание (Б)',
-        'd2': 'География (Б)',
-        'd3': 'МХК (Б)',
-        'd4': 'Теория познания (Б)',
-        'd5': 'Информатика (Б)',
-        'd6': 'Физика (Б)',
-        'd7': 'Решение сложных задач по математике',
-        'd8': 'Информатика в задачах',
-        'd9': 'Сложные вопросы ботаники, зоологии, анатомии',
-        'd10': 'Химические задачи повышенной сложности',
-        'd11': 'Биологический практикум',
-    },
-}
+from forms import models
+
+
+def get_subjects():
+    return {
+        'ГУМ': (
+            (
+                {
+                    'Второй иностранный язык (Б)': models.Gum.objects.filter(d1=True),
+                    'География (Б)': models.Gum.objects.filter(d2=True),
+                    'Обществознание (Б)': models.Gum.objects.filter(d3=True),
+                    'Теория познания (Б)': models.Gum.objects.filter(d4=True),
+                    'Информатика (Б)': models.Gum.objects.filter(d5=True),
+                    'Естествознание (Б)': models.Gum.objects.filter(d6=True),
+                },
+                {
+                    'Зарубежная литература': 1,
+                    'Диалоги с Чеховым': models.Gum.objects.filter(d7=True),
+                    'Современная литература в жанровом аспекте': models.Gum.objects.filter(d8=True),
+                    'Риторика': models.Gum.objects.filter(d9=True),
+                    'Сложные аспекты истории России c древнейших времен до конца XIX века': 1,
+                    'История философии': models.Gum.objects.filter(d10=True),
+                    'История мировой культуры': models.Gum.objects.filter(d11=True),
+                },
+            ),
+            models.Gum.objects.all()
+        ),
+        'СГУМ': (
+            (
+                {
+                    'География (Б)': models.Sgum.objects.filter(d1=True),
+                    'Теория познания (Б)': models.Sgum.objects.filter(d2=True),
+                    'МХК (Б)': models.Sgum.objects.filter(d3=True),
+                    'Информатика (Б)': models.Sgum.objects.filter(d4=True),
+                    'Естествознание (Б)': models.Sgum.objects.filter(d5=True),
+                },
+                {
+                    'Риторика': 1,
+                    'Зарубежная литература': models.Sgum.objects.filter(d6=True),
+                    'Современная литература в жанровом аспекте': models.Sgum.objects.filter(d7=True),
+                    'Диалоги с Чеховым': models.Sgum.objects.filter(d8=True),
+                    'Сложные аспекты истории России c древнейших времен до конца XIX века': 1,
+                    'Политология': models.Sgum.objects.filter(d9=True),
+                    'Социология': models.Sgum.objects.filter(d10=True),
+                    'История философии': models.Sgum.objects.filter(d11=True),
+                },
+            ),
+            models.Sgum.objects.all()
+        ),
+        'СОЦЭК': (
+            (
+                {
+                    'Теория познания (Б)': models.SocEk.objects.filter(d1=True),
+                    'МХК (Б)': models.SocEk.objects.filter(d2=True),
+                    'География (Б)': models.SocEk.objects.filter(d3=True),
+                    'Информатика (Б)': models.SocEk.objects.filter(d4=True),
+                    'Естествознание (Б)': models.SocEk.objects.filter(d5=True),
+                },
+                {
+                    'Экономическая география': models.SocEk.objects.filter(d6=True),
+                    'Экономические задачи повышенной сложности': 1,
+                    'История философии': models.SocEk.objects.filter(d7=True),
+                    'Сложные математические задачи': models.SocEk.objects.filter(d8=True),
+                }
+            ),
+            models.SocEk.objects.all()
+        ),
+        'МИ': (
+            (
+                {
+                    'Обществознание (Б)': models.MatInf.objects.filter(d1=True),
+                    'География (Б)': models.MatInf.objects.filter(d2=True),
+                    'МХК (Б)': models.MatInf.objects.filter(d3=True),
+                    'Теория познания (Б)': models.MatInf.objects.filter(d4=True),
+                    'Биология (Б)': models.MatInf.objects.filter(d5=True),
+                    'Физика (Б)': models.MatInf.objects.filter(d6=True),
+                    'Химия (Б)': models.MatInf.objects.filter(d7=True),
+                },
+                {
+                    'Решение сложных задач по математике': 1,
+                    'Математические основы информатики': 1,
+                    'Избранные главы учебников естественных наук': models.MatInf.objects.filter(d8=True),
+                }
+            ),
+            models.MatInf.objects.all()
+        ),
+        'ФМ': (
+            (
+                {
+                    'Обществознание (Б)': models.FizMat.objects.filter(d1=True),
+                    'География (Б)': models.FizMat.objects.filter(d2=True),
+                    'МХК (Б)': models.FizMat.objects.filter(d3=True),
+                    'Теория познания (Б)': models.FizMat.objects.filter(d4=True),
+                    'Биология (Б)': models.FizMat.objects.filter(d5=True),
+                    'Химия (Б)': models.FizMat.objects.filter(d6=True),
+                },
+                {
+                    'Решение сложных задач по математике': 1,
+                    'Физические задачи повышенной сложности': 1,
+                    'Астрономия': models.FizMat.objects.filter(d7=True),
+                    'Избранные главы учебников естественных наук': models.FizMat.objects.filter(d8=True),
+                }
+            ),
+            models.FizMat.objects.all()
+        ),
+        'ФТ': (
+            (
+                {
+                    'Обществознание (Б)': models.FizTech.objects.filter(d1=True),
+                    'География (Б)': models.FizTech.objects.filter(d2=True),
+                    'МХК (Б)': models.FizTech.objects.filter(d3=True),
+                    'Теория познания (Б)': models.FizTech.objects.filter(d4=True),
+                    'Биология (Б)': models.FizTech.objects.filter(d5=True),
+                    'Химия (Б)': models.FizTech.objects.filter(d6=True),
+                },
+                {
+                    'Решение сложных задач по математике': 1,
+                    'Инженерная графика (геометрическое и проекционное черчение)': models.FizTech.objects.filter(d7=True),
+                    'Физические задачи повышенной сложности': 1,
+                    'Астрономия': models.FizTech.objects.filter(d8=True),
+                    'Избранные главы учебников естественных наук': models.FizTech.objects.filter(d9=True),
+                }
+            ),
+            models.FizTech.objects.all()
+        ),
+        'ХИМ': (
+            (
+                {
+                    'Обществознание (Б)': models.Him.objects.filter(d1=True),
+                    'География (Б)': models.Him.objects.filter(d2=True),
+                    'МХК (Б)': models.Him.objects.filter(d3=True),
+                    'Теория познания (Б)': models.Him.objects.filter(d4=True),
+                    'Информатика (Б)': models.Him.objects.filter(d5=True),
+                    'Биология (Б)': models.Him.objects.filter(d6=True),
+                },
+                {
+                    'Решение сложных задач по математике': models.Him.objects.filter(d7=True),
+                    'Физические задачи повышенной сложности': models.Him.objects.filter(d8=True),
+                    'Биологические задачи': models.Him.objects.filter(d9=True),
+                    'Химические задачи повышенной сложности': 1,
+                    'Химический практикум': models.Him.objects.filter(d10=True),
+                    'Астрономия': models.Him.objects.filter(d11=True),
+                }
+            ),
+            models.Him.objects.all()
+        ),
+        'БИО': (
+            (
+                {
+                    'Обществознание (Б)': models.Bio.objects.filter(d1=True),
+                    'География (Б)': models.Bio.objects.filter(d2=True),
+                    'МХК (Б)': models.Bio.objects.filter(d3=True),
+                    'Теория познания (Б)': models.Bio.objects.filter(d4=True),
+                    'Информатика (Б)': models.Bio.objects.filter(d5=True),
+                    'Физика (Б)': models.Bio.objects.filter(d6=True),
+                },
+                {
+                    'Решение сложных задач по математике': models.Bio.objects.filter(d7=True),
+                    'Информатика в задачах': models.Bio.objects.filter(d8=True),
+                    'Биологические задачи повышенной сложности': 1,
+                    'Сложные вопросы ботаники, зоологии, анатомии': models.Bio.objects.filter(d9=True),
+                    'Химические задачи повышенной сложности': models.Bio.objects.filter(d10=True),
+                    'Биологический практикум': models.Bio.objects.filter(d11=True),
+                }
+            ),
+            models.Bio.objects.all()
+        )
+    }
