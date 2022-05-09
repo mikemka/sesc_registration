68a3bbdac13774b36a539c56d0b1dc9311ff0aae
микемка <mikemka@vk.com>
2022-05-10 01:32:38 +0500

    add pupils to excel export

--- sesc/core/subj_names.py
+++ sesc/core/subj_names.py
@@ -6,163 +6,163 @@ def get_subjects():
         'ГУМ': (
             (
                 {
-                    'Второй иностранный язык (Б)': models.Gum.objects.filter(d1=True),
-                    'География (Б)': models.Gum.objects.filter(d2=True),
-                    'Обществознание (Б)': models.Gum.objects.filter(d3=True),
-                    'Теория познания (Б)': models.Gum.objects.filter(d4=True),
-                    'Информатика (Б)': models.Gum.objects.filter(d5=True),
-                    'Естествознание (Б)': models.Gum.objects.filter(d6=True),
+                    'Второй иностранный язык (Б)': models.Gum.objects.filter(d1=True, attached=True),
+                    'География (Б)': models.Gum.objects.filter(d2=True, attached=True),
+                    'Обществознание (Б)': models.Gum.objects.filter(d3=True, attached=True),
+                    'Теория познания (Б)': models.Gum.objects.filter(d4=True, attached=True),
+                    'Информатика (Б)': models.Gum.objects.filter(d5=True, attached=True),
+                    'Естествознание (Б)': models.Gum.objects.filter(d6=True, attached=True),
                 },
                 {
                     'Зарубежная литература': 1,
-                    'Диалоги с Чеховым': models.Gum.objects.filter(d7=True),
-                    'Современная литература в жанровом аспекте': models.Gum.objects.filter(d8=True),
-                    'Риторика': models.Gum.objects.filter(d9=True),
+                    'Диалоги с Чеховым': models.Gum.objects.filter(d7=True, attached=True),
+                    'Современная литература в жанровом аспекте': models.Gum.objects.filter(d8=True, attached=True),
+                    'Риторика': models.Gum.objects.filter(d9=True, attached=True),
                     'Сложные аспекты истории России c древнейших времен до конца XIX века': 1,
-                    'История философии': models.Gum.objects.filter(d10=True),
-                    'История мировой культуры': models.Gum.objects.filter(d11=True),
+                    'История философии': models.Gum.objects.filter(d10=True, attached=True),
+                    'История мировой культуры': models.Gum.objects.filter(d11=True, attached=True),
                 },
             ),
-            models.Gum.objects.all()
+            models.Gum.objects.filter(attached=True)
         ),
         'СГУМ': (
             (
                 {
-                    'География (Б)': models.Sgum.objects.filter(d1=True),
-                    'Теория познания (Б)': models.Sgum.objects.filter(d2=True),
-                    'МХК (Б)': models.Sgum.objects.filter(d3=True),
-                    'Информатика (Б)': models.Sgum.objects.filter(d4=True),
-                    'Естествознание (Б)': models.Sgum.objects.filter(d5=True),
+                    'География (Б)': models.Sgum.objects.filter(d1=True, attached=True),
+                    'Теория познания (Б)': models.Sgum.objects.filter(d2=True, attached=True),
+                    'МХК (Б)': models.Sgum.objects.filter(d3=True, attached=True),
+                    'Информатика (Б)': models.Sgum.objects.filter(d4=True, attached=True),
+                    'Естествознание (Б)': models.Sgum.objects.filter(d5=True, attached=True),
                 },
                 {
                     'Риторика': 1,
-                    'Зарубежная литература': models.Sgum.objects.filter(d6=True),
-                    'Современная литература в жанровом аспекте': models.Sgum.objects.filter(d7=True),
-                    'Диалоги с Чеховым': models.Sgum.objects.filter(d8=True),
+                    'Зарубежная литература': models.Sgum.objects.filter(d6=True, attached=True),
+                    'Современная литература в жанровом аспекте': models.Sgum.objects.filter(d7=True, attached=True),
+                    'Диалоги с Чеховым': models.Sgum.objects.filter(d8=True, attached=True),
                     'Сложные аспекты истории России c древнейших времен до конца XIX века': 1,
-                    'Политология': models.Sgum.objects.filter(d9=True),
-                    'Социология': models.Sgum.objects.filter(d10=True),
-                    'История философии': models.Sgum.objects.filter(d11=True),
+                    'Политология': models.Sgum.objects.filter(d9=True, attached=True),
+                    'Социология': models.Sgum.objects.filter(d10=True, attached=True),
+                    'История философии': models.Sgum.objects.filter(d11=True, attached=True),
                 },
             ),
-            models.Sgum.objects.all()
+            models.Sgum.objects.filter(attached=True)
         ),
         'СОЦЭК': (
             (
                 {
-                    'Теория познания (Б)': models.SocEk.objects.filter(d1=True),
-                    'МХК (Б)': models.SocEk.objects.filter(d2=True),
-                    'География (Б)': models.SocEk.objects.filter(d3=True),
-                    'Информатика (Б)': models.SocEk.objects.filter(d4=True),
-                    'Естествознание (Б)': models.SocEk.objects.filter(d5=True),
+                    'Теория познания (Б)': models.SocEk.objects.filter(d1=True, attached=True),
+                    'МХК (Б)': models.SocEk.objects.filter(d2=True, attached=True),
+                    'География (Б)': models.SocEk.objects.filter(d3=True, attached=True),
+                    'Информатика (Б)': models.SocEk.objects.filter(d4=True, attached=True),
+                    'Естествознание (Б)': models.SocEk.objects.filter(d5=True, attached=True),
                 },
                 {
-                    'Экономическая география': models.SocEk.objects.filter(d6=True),
+                    'Экономическая география': models.SocEk.objects.filter(d6=True, attached=True),
                     'Экономические задачи повышенной сложности': 1,
-                    'История философии': models.SocEk.objects.filter(d7=True),
-                    'Сложные математические задачи': models.SocEk.objects.filter(d8=True),
+                    'История философии': models.SocEk.objects.filter(d7=True, attached=True),
+                    'Сложные математические задачи': models.SocEk.objects.filter(d8=True, attached=True),
                 }
             ),
-            models.SocEk.objects.all()
+            models.SocEk.objects.filter(attached=True)
         ),
         'МИ': (
             (
                 {
-                    'Обществознание (Б)': models.MatInf.objects.filter(d1=True),
-                    'География (Б)': models.MatInf.objects.filter(d2=True),
-                    'МХК (Б)': models.MatInf.objects.filter(d3=True),
-                    'Теория познания (Б)': models.MatInf.objects.filter(d4=True),
-                    'Биология (Б)': models.MatInf.objects.filter(d5=True),
-                    'Физика (Б)': models.MatInf.objects.filter(d6=True),
-                    'Химия (Б)': models.MatInf.objects.filter(d7=True),
+                    'Обществознание (Б)': models.MatInf.objects.filter(d1=True, attached=True),
+                    'География (Б)': models.MatInf.objects.filter(d2=True, attached=True),
+                    'МХК (Б)': models.MatInf.objects.filter(d3=True, attached=True),
+                    'Теория познания (Б)': models.MatInf.objects.filter(d4=True, attached=True),
+                    'Биология (Б)': models.MatInf.objects.filter(d5=True, attached=True),
+                    'Физика (Б)': models.MatInf.objects.filter(d6=True, attached=True),
+                    'Химия (Б)': models.MatInf.objects.filter(d7=True, attached=True),
                 },
                 {
                     'Решение сложных задач по математике': 1,
                     'Математические основы информатики': 1,
-                    'Избранные главы учебников естественных наук': models.MatInf.objects.filter(d8=True),
+                    'Избранные главы учебников естественных наук': models.MatInf.objects.filter(d8=True, attached=True),
                 }
             ),
-            models.MatInf.objects.all()
+            models.MatInf.objects.filter(attached=True)
         ),
         'ФМ': (
             (
                 {
-                    'Обществознание (Б)': models.FizMat.objects.filter(d1=True),
-                    'География (Б)': models.FizMat.objects.filter(d2=True),
-                    'МХК (Б)': models.FizMat.objects.filter(d3=True),
-                    'Теория познания (Б)': models.FizMat.objects.filter(d4=True),
-                    'Биология (Б)': models.FizMat.objects.filter(d5=True),
-                    'Химия (Б)': models.FizMat.objects.filter(d6=True),
+                    'Обществознание (Б)': models.FizMat.objects.filter(d1=True, attached=True),
+                    'География (Б)': models.FizMat.objects.filter(d2=True, attached=True),
+                    'МХК (Б)': models.FizMat.objects.filter(d3=True, attached=True),
+                    'Теория познания (Б)': models.FizMat.objects.filter(d4=True, attached=True),
+                    'Биология (Б)': models.FizMat.objects.filter(d5=True, attached=True),
+                    'Химия (Б)': models.FizMat.objects.filter(d6=True, attached=True),
                 },
                 {
                     'Решение сложных задач по математике': 1,
                     'Физические задачи повышенной сложности': 1,
-                    'Астрономия': models.FizMat.objects.filter(d7=True),
-                    'Избранные главы учебников естественных наук': models.FizMat.objects.filter(d8=True),
+                    'Астрономия': models.FizMat.objects.filter(d7=True, attached=True),
+                    'Избранные главы учебников естественных наук': models.FizMat.objects.filter(d8=True, attached=True),
                 }
             ),
-            models.FizMat.objects.all()
+            models.FizMat.objects.filter(attached=True)
         ),
         'ФТ': (
             (
                 {
-                    'Обществознание (Б)': models.FizTech.objects.filter(d1=True),
-                    'География (Б)': models.FizTech.objects.filter(d2=True),
-                    'МХК (Б)': models.FizTech.objects.filter(d3=True),
-                    'Теория познания (Б)': models.FizTech.objects.filter(d4=True),
-                    'Биология (Б)': models.FizTech.objects.filter(d5=True),
-                    'Химия (Б)': models.FizTech.objects.filter(d6=True),
+                    'Обществознание (Б)': models.FizTech.objects.filter(d1=True, attached=True),
+                    'География (Б)': models.FizTech.objects.filter(d2=True, attached=True),
+                    'МХК (Б)': models.FizTech.objects.filter(d3=True, attached=True),
+                    'Теория познания (Б)': models.FizTech.objects.filter(d4=True, attached=True),
+                    'Биология (Б)': models.FizTech.objects.filter(d5=True, attached=True),
+                    'Химия (Б)': models.FizTech.objects.filter(d6=True, attached=True),
                 },
                 {
                     'Решение сложных задач по математике': 1,
-                    'Инженерная графика (геометрическое и проекционное черчение)': models.FizTech.objects.filter(d7=True),
+                    'Инженерная графика (геометрическое и проекционное черчение)': models.FizTech.objects.filter(d7=True, attached=True),
                     'Физические задачи повышенной сложности': 1,
-                    'Астрономия': models.FizTech.objects.filter(d8=True),
-                    'Избранные главы учебников естественных наук': models.FizTech.objects.filter(d9=True),
+                    'Астрономия': models.FizTech.objects.filter(d8=True, attached=True),
+                    'Избранные главы учебников естественных наук': models.FizTech.objects.filter(d9=True, attached=True),
                 }
             ),
-            models.FizTech.objects.all()
+            models.FizTech.objects.filter(attached=True)
         ),
         'ХИМ': (
             (
                 {
-                    'Обществознание (Б)': models.Him.objects.filter(d1=True),
-                    'География (Б)': models.Him.objects.filter(d2=True),
-                    'МХК (Б)': models.Him.objects.filter(d3=True),
-                    'Теория познания (Б)': models.Him.objects.filter(d4=True),
-                    'Информатика (Б)': models.Him.objects.filter(d5=True),
-                    'Биология (Б)': models.Him.objects.filter(d6=True),
+                    'Обществознание (Б)': models.Him.objects.filter(d1=True, attached=True),
+                    'География (Б)': models.Him.objects.filter(d2=True, attached=True),
+                    'МХК (Б)': models.Him.objects.filter(d3=True, attached=True),
+                    'Теория познания (Б)': models.Him.objects.filter(d4=True, attached=True),
+                    'Информатика (Б)': models.Him.objects.filter(d5=True, attached=True),
+                    'Биология (Б)': models.Him.objects.filter(d6=True, attached=True),
                 },
                 {
-                    'Решение сложных задач по математике': models.Him.objects.filter(d7=True),
-                    'Физические задачи повышенной сложности': models.Him.objects.filter(d8=True),
-                    'Биологические задачи': models.Him.objects.filter(d9=True),
+                    'Решение сложных задач по математике': models.Him.objects.filter(d7=True, attached=True),
+                    'Физические задачи повышенной сложности': models.Him.objects.filter(d8=True, attached=True),
+                    'Биологические задачи': models.Him.objects.filter(d9=True, attached=True),
                     'Химические задачи повышенной сложности': 1,
-                    'Химический практикум': models.Him.objects.filter(d10=True),
-                    'Астрономия': models.Him.objects.filter(d11=True),
+                    'Химический практикум': models.Him.objects.filter(d10=True, attached=True),
+                    'Астрономия': models.Him.objects.filter(d11=True, attached=True),
                 }
             ),
-            models.Him.objects.all()
+            models.Him.objects.filter(attached=True)
         ),
         'БИО': (
             (
                 {
-                    'Обществознание (Б)': models.Bio.objects.filter(d1=True),
-                    'География (Б)': models.Bio.objects.filter(d2=True),
-                    'МХК (Б)': models.Bio.objects.filter(d3=True),
-                    'Теория познания (Б)': models.Bio.objects.filter(d4=True),
-                    'Информатика (Б)': models.Bio.objects.filter(d5=True),
-                    'Физика (Б)': models.Bio.objects.filter(d6=True),
+                    'Обществознание (Б)': models.Bio.objects.filter(d1=True, attached=True),
+                    'География (Б)': models.Bio.objects.filter(d2=True, attached=True),
+                    'МХК (Б)': models.Bio.objects.filter(d3=True, attached=True),
+                    'Теория познания (Б)': models.Bio.objects.filter(d4=True, attached=True),
+                    'Информатика (Б)': models.Bio.objects.filter(d5=True, attached=True),
+                    'Физика (Б)': models.Bio.objects.filter(d6=True, attached=True),
                 },
                 {
-                    'Решение сложных задач по математике': models.Bio.objects.filter(d7=True),
-                    'Информатика в задачах': models.Bio.objects.filter(d8=True),
+                    'Решение сложных задач по математике': models.Bio.objects.filter(d7=True, attached=True),
+                    'Информатика в задачах': models.Bio.objects.filter(d8=True, attached=True),
                     'Биологические задачи повышенной сложности': 1,
-                    'Сложные вопросы ботаники, зоологии, анатомии': models.Bio.objects.filter(d9=True),
-                    'Химические задачи повышенной сложности': models.Bio.objects.filter(d10=True),
-                    'Биологический практикум': models.Bio.objects.filter(d11=True),
+                    'Сложные вопросы ботаники, зоологии, анатомии': models.Bio.objects.filter(d9=True, attached=True),
+                    'Химические задачи повышенной сложности': models.Bio.objects.filter(d10=True, attached=True),
+                    'Биологический практикум': models.Bio.objects.filter(d11=True, attached=True),
                 }
             ),
-            models.Bio.objects.all()
+            models.Bio.objects.filter(attached=True)
         )
     }
--- sesc/core/views.py
+++ sesc/core/views.py
@@ -60,15 +60,15 @@ def export(request):
     BOOK = xls.Workbook()
     BOOK.remove_sheet(BOOK.active)
     for f_name, full_data in get_subjects().items():
-        data, all_records = full_data
+        data, records = full_data
         sh = BOOK.create_sheet(f_name)
         # Merging
         sh.merge_cells('A2:A3')
         sh.merge_cells('B1:B3')
-        sh.merge_cells(start_row=1, end_row=1, start_column=3, end_column=2 + len(data[0]) + len(data[1]))
+        sh.merge_cells(start_row=1, end_row=1, start_column=3, end_column=2 + (data_len := len(data[0]) + len(data[1])))
         sh.merge_cells(start_row=2, end_row=2, start_column=3, end_column=2 + len(data[0]))
         sh.merge_cells(start_row=2, end_row=2, start_column=3 + len(data[0]),
-                       end_column=2 + len(data[0]) + len(data[1]))
+                       end_column=2 + data_len)
         # Headers
         cell_center_ptserif((
             sh.cell(1, 1, f'Профиль {f_name}'),
@@ -80,16 +80,29 @@ def export(request):
             sh.cell(2, 3, 'Учебные предметы по выбору'),
             sh.cell(2, 3 + len(data[0]), 'Элективные курсы')
         ))
-        cell_borders(summ([[sh.cell(i, 3 + j) for j in range(len(data[0]) + len(data[1]))] for i in (2, 3)]))
+        cell_borders(summ([[sh.cell(i, 3 + j) for j in range(data_len)] for i in (2, 3)]))
         # Add subjects
-        ind, COLORS = 2, ('E1EFDA', 'FFF2CD', 'FFBF00')
+        ind, COLORS = 2, ('E1EFDA', 'FFF2CD', 'FFBF00', 'FF8585')
         for x, i in enumerate(data):
             cell_background((sh.cell(2, ind + 1),), color=COLORS[x])
             for j in i:
                 cell_center_ptserif(cell_background((sh.cell(3, (ind := ind + 1), j),), color=COLORS[x]))
-        
+        # Add pupils
+        for ind, pupil in enumerate(records):
+            cell_background((sh.cell(4 + ind, 2, '-'),))
+            x, checked = 0, False
+            for y, section in enumerate(data):
+                for subject in section:
+                    if section[subject] == 1 or pupil in section[subject]:
+                        if section[subject] != 1:
+                            checked = True
+                        sh.cell(4 + ind, 3 + x, 1)
+                    cell_background((sh.cell(4 + ind, 3 + x),), color=COLORS[y])
+                    x += 1
+            cell_center_ptserif(cell_borders([sh.cell(4 + ind, i) for i in range(1, 3 + data_len)]))
+            cell_background((sh.cell(4 + ind, 1, pupil.name),), color=COLORS[1 if checked else 3]),
         # Other styles
-        col_width([sh.column_dimensions[ALPH[(2 + i) % len(ALPH)]] for i in range(len(data[0]) + len(data[1]))])
+        col_width([sh.column_dimensions[ALPH[(2 + i) % len(ALPH)]] for i in range(data_len)])
         col_width((sh.column_dimensions['A'],), width=24.33)
     # Sending .xlsx
     book_file = NamedTemporaryFile(prefix='exp_', suffix='.xlsx')
